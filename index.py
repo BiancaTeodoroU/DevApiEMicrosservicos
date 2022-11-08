0b6bfae81186002fc0a1850b56c927f85567e216
Bianca Teodoro <101062400+BiancaTeodoroU@users.noreply.github.com>
2022-11-08 14:14:47 -0300

    Add files via upload

---
+++ index.py
@@ -0,0 +1,16 @@
+a []
+
+a.append({'nome': 'bianca', 'date': datetime.datetime.now(), 'tipo': 'N', 'atendido': False, 'posicao': len(a)})
+
+def getPessoa(posicao):
+    pessoa = None
+    for i in a:
+        ifi['posicao'] == posicao:
+            pessoa = i 
+        return pessoa
+    for i in range(index-1, len())
+
+getPessoa()
+    for i in range(index-1, len())
+
+print(getPessoa(0))
